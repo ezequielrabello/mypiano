8e86a1a9f3c0bced1ee7ac1e672911fd4fff8a18
Ezequiel Rabello <rabello950@gmail.com>
2024-01-27 16:21:00 -0300

    deleted everything and started fresh.

--- js/main.js
+++
@@ -1,9 +0,0 @@
-function toggleKeyShow() {
-    var showkeys = window.document.querySelector('label .switch')
-    showkeys.addEventListener('click', () => {
-        showkeys.classList.toggle('keys_active')
-        
-    })
-}
-
-toggleKeyShow()
